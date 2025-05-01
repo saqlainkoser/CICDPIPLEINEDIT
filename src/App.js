a0d22f1fe0ed6ecbe3c68862bab51e8340853aa4
SAQLAIN <saqlain@SAQLAINs-MacBook-Pro.local>
2025-05-01 19:24:01 +0530

    dev changes

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React SHAAAAAAAN BHAI JAAAN
         </a>
       </header>
     </div>
